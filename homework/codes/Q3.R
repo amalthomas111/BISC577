ba2557538fdb39c06b00fea1484bf2f8274975d6
amalthomas111 <amalthomas111@gmail.com>
2016-05-07 19:27:44 -0700

    code updated

--- homework/codes/Q3.R
+++ homework/codes/Q3.R
@@ -81,3 +81,14 @@ print("For max 1-mer")
 print(rs_max)
 print("For max 1-mer+shape")
 print(rs_max1)
+
+x = c(rs_mad,rs_myc,rs_max)
+y = c(rs_mad1,rs_myc1,rs_max1)
+
+#plot(x,y,main="comparison of models",xlab="R^2 sequence model",ylab="R^2 sequence + shape model")#,xlim=c(0.70,0.74), ylim=c(0.70,0.74))
+plot(x[1],y[1],col = "red", pch = 15, xlim=c(0.70,0.75), ylim=c(0.70,0.75),
+     main="comparison of models",xlab="R^2 sequence model",ylab="R^2 sequence + shape model")
+points(x[2],y[2], pch= 16, col="blue")
+points(x[3],y[3],pch =17, col="green")
+abline(lm(y~x), col="black",lty=2)
+legend(0.74,0.745,pch=c(15,16,17),c("Mad","Myc","Max"),col=c('red','blue','green'),bty='n')
